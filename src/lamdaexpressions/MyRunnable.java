a384fa29234475f03eef10e383502b541edcc3f8
LAHARI <LAHARI@LAHARI-PC>
2021-04-08 12:01:14 +0530

    Lamda Thread

---
+++ src/lamdaexpressions/MyRunnable.java
@@ -0,0 +1,17 @@
+package lamdaexpressions;
+
+public class MyRunnable implements Runnable { 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=0; i<5; i++) {
+		System.out.println("in myrunnable thread");
+	}
+	}
+	/*
+	 * //()-> { for(int i=0; i<5; i++) { System.out.println("in the thread"); }
+	 * 
+	 * }
+	 */
+
+}
