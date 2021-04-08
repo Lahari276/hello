9738e5d94e2d0823a21aee42a3fa0a205e7747fa
LAHARI <LAHARI@LAHARI-PC>
2021-04-08 14:40:30 +0530

    ascending order of lists in employee comparator

--- src/lamdaexpressions/collections/TestEmployees.java
+++ src/lamdaexpressions/collections/TestEmployees.java
@@ -15,8 +15,9 @@ public class TestEmployees {
 		
 		System.out.println(employees);
 		Comparator<Employee> c = (e1,e2)-> (e1.eid <e2.eid)? -1 : (e1.eid >e2.eid)? 1 : 0;
-		
+		Comparator<Employee> n = (e1,e2)-> (e1.name.compareTo(e2.name));
 		Collections.sort(employees,c);
+		Collections.sort(employees,n);
 		System.out.println(employees);
 	}
 
